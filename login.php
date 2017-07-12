1eca913ed1016d29d002707f5f113014f05e195f
Mayco Git <mayco.git@gmail.com>
2017-07-12 14:40:23 -0300

    login.php continuação

--- login.php
+++ login.php
@@ -1,6 +1,14 @@
 <?php
 
+// obtém os dados digitados
 $email = $_POST["email"];
 $senha = $_POST["senha"];
+
+// acesso ao banco de dados
+include "conecta_mysql.inc";
+$resultado = mysql_query("SELECT * FROM usuarios WHERE email");
+$linhas = mysql_num_rows($resultado);
+
+
 
 ?>
