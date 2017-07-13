e2b6f55c996ba8d71d457b4bdafd830614ca1d78
Mayco Git <mayco.git@gmail.com>
2017-07-13 08:35:38 -0300

    rename index.html > index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<?php include "charset_utf8.inc"; ?>
+<title><?php include "titulo_geral.inc"; ?></title>
+</head>
+<body>
+	<h1>Radar</h1>
+	<form method="POST" action="login.php">
+		<p>E-mail <input type="text" name="email" /></p>
+		<p>Senha <input type="senha" name="senha" /></p>
+		<p><input type="submit" value="Entrar" name="Entrar" /></p>
+	</form>
+</body>
+</html>
