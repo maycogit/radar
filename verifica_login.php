754a7e1879dcf99655a5271dd138df83eb351899
Mayco Git <mayco.git@gmail.com>
2017-07-13 09:27:56 -0300

    verifica_login.php

--- verifica_login.php
+++ verifica_login.php
@@ -9,21 +9,15 @@ include "conecta_mysql.inc";
 $resultado = mysql_query("SELECT * FROM usuarios WHERE email='$email'");
 $linhas = mysql_num_rows($resultado);
 if($linhas ==0) {
-	echo "<html><body>";
-	echo "<p>Usuário não encontrado!</p>";
-	echo "<p><a href=\"index.html\">Voltar</a></p>";
-	echo "</html></body>";
+	include "usuario_nao_encontrado.inc";
 } else {
 	if($senha != mysql_result($resultado, 0, "senha")) { // confere senha
-		echo "<html><body>";
-		echo "<p>Senha incorreta!</p>";
-		echo "<p><a href=\"index.html\">Voltar</a></p>";
-		echo "</html></body>";
+		include "senha_incorreta.inc";
 	} else { // usuário e senha corretos, criando cookies
 		setcookie("email_usuario", $email);
 		setcookie("senha_usuario", $senha);
 		// direciona para a página inicial dos usuários cadastrados
-		header("Location: pagina_inicial.php");
+		header("Location: radar.php");
 	}
 }
 mysql_close($conexao);
