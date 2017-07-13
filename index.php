548b999b4b135f8fb79626f95157643aed829925
Mayco Git <mayco.git@gmail.com>
2017-07-13 08:58:14 -0300

    index.html Login Radar

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="<?php include "html_lang.inc"; ?>">
 <head>
-<?php include "conecta_mysql.inc"; ?>
 <meta charset="<?php include "charset_utf8.inc"; ?>">
 <title><?php include "titulo_geral.inc"; ?></title>
 </head>
 <body>
-	<h1>Radar</h1>
+	<h1>Login Radar</h1>
 	<form method="POST" action="login.php">
 		<p>E-mail <input type="text" name="email" /></p>
 		<p>Senha <input type="senha" name="senha" /></p>
