34060560e81d0e76420a31baa350aea3d339fe35
Mayco Git <mayco.git@gmail.com>
2017-07-13 09:39:30 -0300

    banco_dados.sql usuários para testes

--- banco_dados.sql
+++ banco_dados.sql
@@ -2,9 +2,13 @@ CREATE DATABASE radar;
 
 USE radar;
 
+/* tabela de usuários para login */
 CREATE TABLE usuarios
 (
 	email varchar(80) NOT NULL,
 	senha varchar(20) NOT NULL,
 	primary key(email)
 );
+
+/* usuários para testes */
+INSERT INTO usuarios VALUES ( 'a', 'b' );
