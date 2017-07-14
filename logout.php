46a3f0eac9df5ce5b9797593497f102f511c1402
Mayco Git <mayco.git@gmail.com>
2017-07-14 08:05:37 -0300

    logout.php correção no header

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 setcookie("email_usuario");
 setcookie("senha_usuario");
-header("Location login.php"); // usar aqui o include "pagina_login.inc";
+header("Location: login.php"); // usar aqui o include "pagina_login.inc";
 ?>
