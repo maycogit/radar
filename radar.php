70f4c87dc8e444f6397ab60a6a94a12c78713109
Mayco Git <mayco.git@gmail.com>
2017-07-14 08:40:54 -0300

    radar.php

---
+++ radar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?php include "html_lang.inc"; ?>">
+<head>
+<meta charset="<?php include "charset_utf8.inc"; ?>">
+<title><?php include "titulo_geral.inc"; ?></title>
+</head>
+<body>
+	<?php include "valida_cookies.inc"; ?>
+	<h1>Radar</h1>
+	<p><a href="<?php include "pagina_logout.inc"; ?>">Sair</a></p>
+</body>
+</html>
