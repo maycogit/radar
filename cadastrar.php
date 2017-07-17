f42830c627358f489fabdc2ed48624eab94a758f
Mayco Git <mayco.git@gmail.com>
2017-07-17 08:47:08 -0300

    cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="<?php include "html_lang.inc"; ?>">
+<head>
+<?php include "html_head_geral.inc"; ?>
+</head>
+<body>
+<?php include "valida_cookies.inc"; ?>
+<?php include "menu_principal.inc"; ?>
+</body>
+</html>
