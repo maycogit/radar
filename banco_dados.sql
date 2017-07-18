02dea0c495c95d822d4628859b909bb393244c6a
Mayco Git <mayco.git@gmail.com>
2017-07-18 09:40:26 -0300

    banco_dados.sql tabela ensalamento

--- banco_dados.sql
+++ banco_dados.sql
@@ -5,9 +5,9 @@ USE radar;
 /* tabela de usuários para login */
 CREATE TABLE usuarios
 (
-	email varchar(80) NOT NULL,
-	senha varchar(20) NOT NULL,
-	primary key(email)
+	email VARCHAR NOT NULL,
+	senha VARCHAR NOT NULL,
+	PRIMARY KEY(email)
 );
 
 /* tabela das salas */
@@ -20,9 +20,19 @@ CREATE TABLE salas
 }
 
 /* tabela para reserva das salas */
-CREATE TABLE reserva_salas
+CREATE TABLE ensalamento
 {
-	
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	email_usuario VARCHAR NOT NULL,
+	dt_solicitacao DATETIME,
+	dt_inicio DATETIME,
+	dt_fim DATETIME,
+	dt_cancelamento DATETIME,
+	cancelado BOOLEAN,
+	excecao BOOLEAN, /* TRUE significa uma reserva extraordináia. FALSE siguinifica ensalamento de rotina semanal */
+	id_sala INT UNSIGNED,
+	solicitante VARCHAR,
+	PRIMARY KEY(id)
 }
 
 /* usuários para testes */
